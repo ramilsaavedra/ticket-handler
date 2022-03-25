4cba6fcf1395a7f4931327c6c6d37c12dc28e941
Ramil Saavedra <ramilsmcl@gmail.com>
2022-03-25 16:17:21 +0800

    user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,12 @@
 const registerUser = (req, res) => {
   res.send('Regiter Route')
 }
+
+const loginUser = (req, res) => {
+  res.send('Login Route')
+}
+
+module.exports = {
+  registerUser,
+  loginUser,
+}
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
+const { registerUser, loginUser } = require('../controllers/userController')
 
-router.post('/', (req, res) => {
+router.post(registerUser, (req, res) => {
   res.send('Register Route')
 })
 
-router.post('/login', (req, res) => {
+router.post(loginUser, (req, res) => {
   res.send('Login Route')
 })
 
