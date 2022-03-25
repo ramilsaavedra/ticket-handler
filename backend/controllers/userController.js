a61515004ef131c3ae4c6476a738b6ea0eca1f95
Ramil Saavedra <ramilsmcl@gmail.com>
2022-03-25 16:22:13 +0800

    fix user routes error

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,15 @@
+// @desc Register a new user
+// @route /api/users
+// @access Public
 const registerUser = (req, res) => {
+  console.log(req.body)
+
   res.send('Regiter Route')
 }
 
+// @desc Login user
+// @routes /api/users/login
+// @access Public
 const loginUser = (req, res) => {
   res.send('Login Route')
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,12 +2,8 @@ const express = require('express')
 const router = express.Router()
 const { registerUser, loginUser } = require('../controllers/userController')
 
-router.post(registerUser, (req, res) => {
-  res.send('Register Route')
-})
+router.post('/', registerUser)
 
-router.post(loginUser, (req, res) => {
-  res.send('Login Route')
-})
+router.post('/login', loginUser)
 
 module.exports = router
