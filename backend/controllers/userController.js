33993df2278a127dd3f02ac1e34db1d2a947b193
Ramil Saavedra <ramilsmcl@gmail.com>
2022-03-25 16:52:34 +0800

    fix error

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,7 +2,7 @@ const asyncHandler = require('express-async-handler')
 // @desc Register a new user
 // @route /api/users
 // @access Public
-const registerUser = asyncHandler(async(req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body
 
   // Validation
@@ -17,11 +17,11 @@ const registerUser = asyncHandler(async(req, res) => {
 // @desc Login user
 // @routes /api/users/login
 // @access Public
-const loginUser = asyncHandler(async(req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
   res.send('Login Route')
-}
+})
 
 module.exports = {
   registerUser,
   loginUser,
-})
+}
