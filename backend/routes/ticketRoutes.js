fb91632f8a6c81e783a09bc53d1c495eeac1406e
Ramil Saavedra <ramilsmcl@gmail.com>
2022-04-04 12:45:21 +0800

    init ticket api

---
+++ backend/routes/ticketRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+const {
+  getTickets,
+  createTicket,
+} = require('../controllers/ticketController.js')
+const { protect } = require('../middleware/authMiddleware')
+
+router.route('/').get(protect, getTickets).post(protect, createTicket)
+
+module.exports = router
