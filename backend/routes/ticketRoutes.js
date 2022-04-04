da594c3116999a80167f62a5603ead0b22ed6b36
Ramil Saavedra <ramilsmcl@gmail.com>
2022-04-04 13:19:40 +0800

    crud ticket

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -3,9 +3,18 @@ const router = express.Router()
 const {
   getTickets,
   createTicket,
+  getTicket,
+  deleteTicket,
+  updateTicket,
 } = require('../controllers/ticketController.js')
 const { protect } = require('../middleware/authMiddleware')
 
 router.route('/').get(protect, getTickets).post(protect, createTicket)
+
+router
+  .route('/:id')
+  .get(protect, getTicket)
+  .delete(protect, deleteTicket)
+  .put(protect, updateTicket)
 
 module.exports = router
