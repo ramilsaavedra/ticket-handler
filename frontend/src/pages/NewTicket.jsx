cb657342fcdaeaae9a93228f5f6aa4e767626ce1
Ramil Saavedra <ramilsmcl@gmail.com>
2022-04-04 18:37:01 +0800

    create ticket action

--- frontend/src/pages/NewTicket.jsx
+++ frontend/src/pages/NewTicket.jsx
@@ -1,15 +1,43 @@
-import { useState } from 'react'
-import { useSelector } from 'react-redux'
+import { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { createTicket, reset } from '../features/tickets/ticketSlice'
+import Spinner from '../components/Spinner'
 
 const NewTicket = () => {
   const { user } = useSelector((state) => state.auth)
+  const { isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.ticket
+  )
   const [name] = useState(user.name)
   const [email] = useState(user.email)
   const [product, setProduct] = useState('')
   const [description, setDescription] = useState('')
 
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message)
+    }
+
+    if (isSuccess) {
+      dispatch(reset())
+      navigate('/tickets')
+    }
+
+    dispatch(reset())
+  }, [dispatch, isError, isSuccess, navigate, message])
+
   const onSubmit = (e) => {
     e.preventDefault()
+    dispatch(createTicket({ product, description }))
+  }
+
+  if (isLoading) {
+    return <Spinner />
   }
 
   return (
@@ -52,7 +80,13 @@ const NewTicket = () => {
               value={description}
               className='form-control'
               onChange={(e) => setDescription(e.target.value)}
+              placeholder='Description'
             ></textarea>
+          </div>
+          <div className='form-group'>
+            <button className='btn btn-block' type='submit'>
+              Submit
+            </button>
           </div>
         </form>
       </section>
