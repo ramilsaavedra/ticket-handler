17ac253d8b7990bf7c8188e6a82c2c5378b1d270
Ramil Saavedra <ramilsmcl@gmail.com>
2022-04-04 15:03:52 +0800

    private route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import PrivateRoute from './components/PrivateRoute'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import Home from './pages/Home'
 import Register from './pages/Register'
 import Login from './pages/Login'
 import Header from './components/Header'
+import NewTicket from './pages/NewTicket'
 
 function App() {
   return (
@@ -17,6 +19,9 @@ function App() {
             <Route path='/' element={<Home />} />
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
+            <Route path='/new-ticket' element={<PrivateRoute />}>
+              <Route path='/new-ticket' element={<NewTicket />} />
+            </Route>
           </Routes>
         </div>
       </Router>
