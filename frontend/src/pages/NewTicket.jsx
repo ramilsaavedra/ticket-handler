19a9869348a568f6244549461d35e4d6d1c06279
Ramil Saavedra <ramilsmcl@gmail.com>
2022-04-04 17:51:44 +0800

    new ticket form

--- frontend/src/pages/NewTicket.jsx
+++ frontend/src/pages/NewTicket.jsx
@@ -1,4 +1,62 @@
+import { useState } from 'react'
+import { useSelector } from 'react-redux'
+
 const NewTicket = () => {
-  return <div>NewTicket</div>
+  const { user } = useSelector((state) => state.auth)
+  const [name] = useState(user.name)
+  const [email] = useState(user.email)
+  const [product, setProduct] = useState('')
+  const [description, setDescription] = useState('')
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  return (
+    <>
+      <section className='heading'>
+        <h1>Create New Ticket</h1>
+        <p>Please fill out the form below</p>
+      </section>
+      <section className='form'>
+        <div className='form-group'>
+          <label htmlFor='name'>Customer Name</label>
+          <input type='text' className='form-control' value={name} disabled />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='email'>Customer Email</label>
+          <input type='text' className='form-control' value={email} disabled />
+        </div>
+        <form onSubmit={onSubmit}>
+          <div className='form-group'>
+            <label htmlFor='product' className='product'>
+              Product
+            </label>
+            <select
+              name='product'
+              id='product'
+              value={product}
+              onChange={(e) => setProduct(e.target.value)}
+            >
+              <option value='iPhone'>iPhone</option>
+              <option value='iMac'>iMac</option>
+              <option value='iPad'>iPad</option>
+              <option value='Macbook Pro'>Macbook Pro</option>
+            </select>
+          </div>
+          <div className='form-group'>
+            <label htmlFor='description'>Description of the issue</label>
+            <textarea
+              name='description'
+              id='description'
+              value={description}
+              className='form-control'
+              onChange={(e) => setDescription(e.target.value)}
+            ></textarea>
+          </div>
+        </form>
+      </section>
+    </>
+  )
 }
 export default NewTicket
